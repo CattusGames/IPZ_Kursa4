d02b75ae48e52af85ebf1486e47e268f4a6876d8
CattusGames <cattusgames@gmail.com>
2023-01-11 21:57:55 +0200

    Class triangle

--- Kursa4/Form1.cs
+++ Kursa4/Form1.cs
@@ -14,40 +14,52 @@ namespace Kursa4
     public partial class Form1 : Form
     {
         string line;
-        StreamReader sr = new StreamReader(@"C:\Users\taras\OneDrive\Документы\GitHub\IPZ_Kursa4\coords.txt");
+        StreamReader sr = new StreamReader(@"C:\Users\ASUS\Desktop\Git\Lab_Web\IPZ_Kursa4\coords.txt");
         List<string> ls = new List<string>();
         string finalLine;
+        
         public Form1()
         {
+            Graphics gr = CreateGraphics();
+
             do
             {
                 line = sr.ReadLine();
                 ls.Add(line);
             } while (line != null);
             sr.Close();
-            Tochka[] mt = new Tochka[ls.Count - 1];
+            Triangle[] mt = new Triangle[ls.Count - 1];
             for (int i = 0; i < mt.Length; i++)
             {
                 char[] seps = { ' ', ':', ',', ';', '=' };
                 string[] ms = ls[i].Split(seps, StringSplitOptions.RemoveEmptyEntries);
-                mt[i] = new Tochka() { x = int.Parse(ms[3]), y = int.Parse(ms[5]) };
+                
+                mt[i] = new Triangle() {
+                    first = new Point(int.Parse(ms[3]), int.Parse(ms[5])),
+                    second = new Point(int.Parse(ms[7]), int.Parse(ms[9])),
+                    third = new Point(int.Parse(ms[11]), int.Parse(ms[13])),
+                };
             }
-            foreach (Tochka t in mt)
+            foreach (Triangle t in mt)
             {
                 //Console.WriteLine($"x={t.x}, y={t.y}");
-                finalLine += "Point: X: "+t.x+" Y: "+t.y+" \n";
+                finalLine += "Point: X1: "+t.first.X+" Y1: "+t.first.Y+ " X2: " + t.second.X + " Y2: " + t.second.Y + " X3: " + t.third.X + " Y3: " + t.third.Y + "\n";
             }
+            
             InitializeComponent();
         }
-
+        
         public void textBox1_TextChanged(object sender, EventArgs e)
         {
+
             textBox1.Text = finalLine;
         }
-        class Tochka
+        public class Triangle
         {
-            public int x { get; set; }
-            public int y { get; set; }
+            public Point first { get; set; }
+            public Point second { get; set; }
+            public Point third { get; set; }
         }
+
     }
 }
