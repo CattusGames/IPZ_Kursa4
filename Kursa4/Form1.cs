00e306ded64f2bc1d48f35e843628b71c1ae0110
CattusGames <67207327+CattusGames@users.noreply.github.com>
2023-01-11 17:44:31 +0200

    Complete file IO

--- Kursa4/Form1.cs
+++ Kursa4/Form1.cs
@@ -7,14 +7,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Kursa4
 {
     public partial class Form1 : Form
     {
+        string line;
+        StreamReader sr = new StreamReader(@"C:\Users\taras\OneDrive\Документы\GitHub\IPZ_Kursa4\coords.txt");
+        List<string> ls = new List<string>();
+        string finalLine;
         public Form1()
         {
+            do
+            {
+                line = sr.ReadLine();
+                ls.Add(line);
+            } while (line != null);
+            sr.Close();
+            Tochka[] mt = new Tochka[ls.Count - 1];
+            for (int i = 0; i < mt.Length; i++)
+            {
+                char[] seps = { ' ', ':', ',', ';', '=' };
+                string[] ms = ls[i].Split(seps, StringSplitOptions.RemoveEmptyEntries);
+                mt[i] = new Tochka() { x = int.Parse(ms[3]), y = int.Parse(ms[5]) };
+            }
+            foreach (Tochka t in mt)
+            {
+                //Console.WriteLine($"x={t.x}, y={t.y}");
+                finalLine += "Point: X: "+t.x+" Y: "+t.y+" \n";
+            }
             InitializeComponent();
+        }
+
+        public void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = finalLine;
+        }
+        class Tochka
+        {
+            public int x { get; set; }
+            public int y { get; set; }
         }
     }
 }
--- Kursa4/Program.cs
+++ Kursa4/Program.cs
@@ -9,37 +9,13 @@ namespace Kursa4
 {
     internal static class Program
     {
-       static string line;
-       static StreamReader sr = new StreamReader(@"E:\My1.txt");
-       static List<string> ls = new List<string>();
-
-[STAThread]
+        [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            do
-            {
-                line = sr.ReadLine();
-                ls.Add(line);
-            } while (line != null);
-            sr.Close();
-            Tochka[] mt = new Tochka[ls.Count - 1];
-            for (int i = 0; i < mt.Length; i++)
-            {
-                char[] seps = { ' ', ':', ',', ';', '=' };
-                string[] ms = ls[i].Split(seps, StringSplitOptions.RemoveEmptyEntries);
-                mt[i] = new Tochka() { x = int.Parse(ms[3]), y = int.Parse(ms[5]) };
-            }
-            foreach (Tochka t in mt)
-                Console.WriteLine($"x={t.x}, y={t.y}");
-
         }
     }
-class Tochka
-{
-    public int x { get; set; }
-    public int y { get; set; }
-}
+
 }
